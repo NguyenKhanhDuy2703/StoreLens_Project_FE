30fbb4572947835818741c813f66a1d8a41db978
vothituongvy <Vothituongvyvttv@gmail.com>
2025-08-20 22:51:12 +0700

    feat(Heatmap): Add feature for website

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import MovementFlow from "../features/MovementFlow/MovementFlow";
 import Dashboard from "../features/Dashboard/Dashboard";
+import Heatmap from "../features/Heatmap/Heatmap";
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<div>Home Page</div>} />
       <Route path="/MovementFlow" element={<MovementFlow />} />
       <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path="/Heatmap" element={<Heatmap />} />
     </Routes>
   );
 };
